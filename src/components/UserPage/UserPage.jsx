e247738706ccbbf15491826ea677b56447d5cd69
randomnewb <nguy1581@gmail.com>
2023-05-06 12:56:35 -0500

    Co-authored-by: Nathan Gaede <nathan-gaede@users.noreply.github.com>
    Implemented
    - Add task
    - Delete task

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,18 +1,90 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import React from "react";
+import { useState } from "react";
+
+let initialTasks = [
+  {
+    id: 1,
+    detail: "take out trash",
+    completed: false,
+  },
+  {
+    id: 2,
+    detail: "do dishes",
+    completed: false,
+  },
+];
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
+  const [tasks, setTasks] = useState(initialTasks);
+  const [taskDetail, setTaskDetail] = useState("");
+
+  const addTask = (taskDetail) => {
+    setTaskDetail("");
+
+    if (tasks.length === 0) {
+      setTasks([
+        {
+          id: 1,
+          detail: taskDetail,
+          completed: false,
+        },
+      ]);
+    } else {
+      setTasks([
+        ...tasks,
+        {
+          id: tasks[tasks.length - 1].id + 1,
+          detail: taskDetail,
+          completed: false,
+        },
+      ]);
+    }
+  };
+
+  const deleteTask = (id) => {
+    const check = confirm("Delete task?");
+
+    if (check) {
+      setTasks(tasks.filter((task) => task.id !== id));
+    }
+  };
+
+  const completeTask = (id) => {
+    console.log("in completeTask");
+  };
+
+  const editTask = (id) => {
+    console.log("in editTask");
+    console.log(id);
+  };
+
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <input
+        value={taskDetail}
+        onChange={(e) => setTaskDetail(e.target.value)}
+      />
+      {/* Add task */}
+      <button onClick={() => addTask(taskDetail)}> Add task</button>
+      <br />
+      <br />
+      Tasks
+      <br />
+      <br />
+      {tasks.map((task) => (
+        <>
+          <div>
+            {task.detail} ID: {task.id}
+            <button onClick={() => deleteTask(task.id)}>Delete task</button>
+            <button onClick={() => editTask(task.id)}>Edit task</button>
+            <button onClick={() => completeTask(task.id)}>
+              Task completed
+            </button>
+          </div>
+        </>
+      ))}
     </div>
   );
 }
-
 // this allows us to use <App /> in index.js
 export default UserPage;
